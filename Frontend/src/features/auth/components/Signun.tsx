25701db2b002a1ce10b19d4c4d6b5b2254aea968
AbdulRahman <anuzainbzain@gmail.com>
2025-08-11 10:20:02 +0530

    validation update

--- Frontend/src/features/auth/components/Signun.tsx
+++ Frontend/src/features/auth/components/Signun.tsx
@@ -39,7 +39,13 @@ export const SignupPage: React.FC = () => {
     phone: Yup.string()
       .matches(/^\+?[1-9]\d{1,14}$/, "Phone number is not valid")
       .required("Phone number is required"),
-    password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+   password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+    .matches(/[a-z]/, "Password must contain at least one lowercase letter")
+    .matches(/[0-9]/, "Password must contain at least one number")
+    .matches(/[@$!%*?&]/, "Password must contain at least one special character (@$!%*?&)")
+    .required("Password is required"),
     confirmPassword: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords must match")
     .required("Confirm Password is required"),
